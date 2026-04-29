7eb87ef196fbdb0155dca0ad19743629c966d303
Alijah00 <alijahtan05@gmail.com>
2026-04-29 08:30:30 +0700

    Retry.cs

---
+++ 229F-Death-star/Assets/Script/Retry.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Retry : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            RetryGame();
+        }
+    }
+
+    public void RetryGame()
+    {
+    
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
